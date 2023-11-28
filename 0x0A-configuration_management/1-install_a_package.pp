b99c889cb6cdb05c233e504832bdeddd0cf92bb9
Aymen Idouahman <aymanidou@gmail.com>
2023-11-28 21:13:31 +0100

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -6,7 +6,7 @@ class install_flask {
   }
 
   exec { 'install_flask':
-    command => '/usr/bin/pip3 install Flask',
+    command => '/usr/bin/pip3 install Flask==2.1.0',
     path    => ['/usr/bin'],
     require => Package['python3-pip'],
   }
