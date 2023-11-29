da1ecea65008ac18cff4425136913d5007e440d4
Aymen Idouahman <aymanidou@gmail.com>
2023-11-29 12:28:07 +0100

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,9 @@
 #  install flask from pip3 using puppet
 package { 'python3-pip':
-  ensure   => installed,
+  ensure => installed,
 }
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin'],
+  require => Package['python3-pip'],
 }
