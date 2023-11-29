35126dbbfd4d08ea05201ba4901a9f7fc86bad0e
Aymen Idouahman <aymanidou@gmail.com>
2023-11-29 11:12:46 +0100

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,16 +1,8 @@
 #  install flask from pip3 using puppet
-# Define a class for installing Flask
-class install_flask {
-  package { 'python3-pip':
-    ensure => installed,
-  }
-
-  exec { 'install_flask':
-    command => '/usr/bin/pip3 install Flask==2.1.0',
-    path    => ['/usr/bin'],
-    require => Package['python3-pip'],
-  }
+package { 'python3-pip':
+  ensure   => installed,
 }
-
-# Include the class in the main manifest
-include install_flask
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
