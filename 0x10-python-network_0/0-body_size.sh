b4864f9902836eac074ce7291eb507867bf558fe
Aymen Idouahman <aymanidou@gmail.com>
2023-12-02 02:13:05 +0100

    pushing to wrong repo

--- 0x10-python-network_0/0-body_size.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# store the url in a variable
-url=$1
-
-# Use curl to send a request and capture the response body size in bytes
-response_size=$(curl -sI "$url" | grep -i content-length | awk '{print $2}' | tr -d '\r\n')
-echo "${response_size}"
