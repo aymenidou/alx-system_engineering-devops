801daecd01387cef1613f41ed4f6d89b2b3613ad
Aymen Idouahman <aymanidou@gmail.com>
2024-01-03 00:36:16 +0100

    1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" gather data from an API """
+import requests
+import sys
+import csv
+
+
+if __name__ == '__main__':
+    url_users = "https://jsonplaceholder.typicode.com/users/{}".format(
+        sys.argv[1])
+    users_response = requests.get(url_users)
+    username = users_response.json().get('name')
+    url_todos = "https://jsonplaceholder.typicode.com/todos/"
+    todos_response = requests.get(url_todos).json()
+    completed_tasks = 0
+    tasks = 0
+    task_desk = []
+    filename = sys.argv[1] + '.csv'
+    with open(filename, "w") as file:
+        write = csv.writer(file, quoting=csv.QUOTE_ALL, delimiter=',')
+        for todo in todos_response:
+            if (todo.get('userId') == int(sys.argv[1])):
+                row = [todo.get('userId'),
+                       username,
+                       str(todo.get('completed')),
+                       todo.get('title')]
+                write.writerow(row)
