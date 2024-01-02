8f51912693fc901b01356d479369773a3dbef503
Aymen Idouahman <aymanidou@gmail.com>
2024-01-03 00:38:29 +0100

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,19 +9,20 @@ if __name__ == '__main__':
     url_users = "https://jsonplaceholder.typicode.com/users/{}".format(
         sys.argv[1])
     users_response = requests.get(url_users)
-    username = users_response.json().get('name')
-    url_todos = "https://jsonplaceholder.typicode.com/todos/"
-    todos_response = requests.get(url_todos).json()
-    completed_tasks = 0
-    tasks = 0
-    task_desk = []
-    filename = sys.argv[1] + '.csv'
-    with open(filename, "w") as file:
-        write = csv.writer(file, quoting=csv.QUOTE_ALL, delimiter=',')
-        for todo in todos_response:
-            if (todo.get('userId') == int(sys.argv[1])):
-                row = [todo.get('userId'),
-                       username,
-                       str(todo.get('completed')),
-                       todo.get('title')]
-                write.writerow(row)
+    if(users_response.status_code==200):
+        username = users_response.json().get('name')
+        url_todos = "https://jsonplaceholder.typicode.com/todos/"
+        todos_response = requests.get(url_todos).json()
+        completed_tasks = 0
+        tasks = 0
+        task_desk = []
+        filename = sys.argv[1] + '.csv'
+        with open(filename, "w") as file:
+            write = csv.writer(file, quoting=csv.QUOTE_ALL, delimiter=',')
+            for todo in todos_response:
+                if (todo.get('userId') == int(sys.argv[1])):
+                    row = [todo.get('userId'),
+                        username,
+                        str(todo.get('completed')),
+                        todo.get('title')]
+                    write.writerow(row)
