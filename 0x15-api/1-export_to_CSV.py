13edaa761d68f0e27a1e6c8385f0cddfd7f22e8e
Aymen Idouahman <aymanidou@gmail.com>
2024-01-04 15:05:54 +0100

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,12 +10,9 @@ if __name__ == '__main__':
         sys.argv[1])
     users_response = requests.get(url_users)
     if (users_response.status_code == 200):
-        username = users_response.json().get('name')
+        username = users_response.json().get('username')
         url_todos = "https://jsonplaceholder.typicode.com/todos/"
         todos_response = requests.get(url_todos).json()
-        completed_tasks = 0
-        tasks = 0
-        task_desk = []
         filename = sys.argv[1] + '.csv'
         with open(filename, "w") as file:
             write = csv.writer(file, quoting=csv.QUOTE_ALL, delimiter=',')
