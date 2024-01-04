0d5aaca45003557a20d2719b62a01b16c2998b03
Aymen Idouahman <aymanidou@gmail.com>
2024-01-04 16:08:09 +0100

    2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ gather data from an API """
+import json
 import requests
 import sys
-import json
 
 if __name__ == '__main__':
     url_users = "https://jsonplaceholder.typicode.com/users/{}".format(
