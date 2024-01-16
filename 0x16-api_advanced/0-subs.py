1010ff14beb7154defa5c144d9156107a172bcf0
Aymen Idouahman <aymanidou@gmail.com>
2024-01-17 00:25:38 +0100

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import requests
+
+
+def get_subreddit_subscribers(subreddit):
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'custom_user_agent'}
+
+    response = requests.get(url, headers=headers)
+    data = response.json()
+
+    subscribers = data.get('data', {}).get('subscribers', 0)
+    return subscribers
