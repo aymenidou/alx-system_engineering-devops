c37d5a13a15c786289cf418db49db848c9d04a9d
Aymen Idouahman <aymanidou@gmail.com>
2024-02-24 20:09:15 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,22 +6,23 @@ from requests import get
 def number_of_subscribers(subreddit):
     """fetch subressit subscribers"""
 
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'user-agent': 'custom_user_agent'}
+    if subreddit :
+        url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+        headers = {'user-agent': 'custom_user_agent'}
 
-    res = get(url, headers=headers, allow_redirects=False)
+        res = get(url, headers=headers, allow_redirects=False)
 
-    if res.status_code != 200:
-        return 0
+        if res.status_code != 200:
+            return 0
 
-    try:
-        js = res.json()
-    except ValueError:
-        return 0
-    data = js.get("data")
+        try:
+            js = res.json()
+        except ValueError:
+            return 0
+        data = js.get("data")
 
-    if data:
-        sub_count = data.get("subscribers")
-        if sub_count:
-            return sub_count
+        if data:
+            sub_count = data.get("subscribers")
+            if sub_count:
+                return sub_count
     return 0
